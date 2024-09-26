e06a2e4b7fd013eae1c5c1e0352ee0dd593342c0
ITU-MOSHIA <moshiaitumeleng6@gmail.com>
2024-09-26 13:41:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using ConsoleApp35;
+
+namespace ConsoleApp35
+{
+    class BeerSong
+    {
+        public void Sing()
+        {
+            for (int Bottles = 99; Bottles > 0; Bottles--) 
+            {
+                Console.WriteLine($"{Bottles} Bottles{(Bottles > 1 ? "s" : "")} of beer on the wall, {Bottles} Bottles{(Bottles > 1 ? "s" : "")} of beer.");
+                Console.WriteLine("Take one down and pass it around,");
+                
+            }
+            Console.WriteLine("No more Bottles of beer on the wall, no more Bottles of beer.");
+        }
+    }
+}
+class Program
+{
+    static void Main() 
+    {
+        var song = new BeerSong();
+        song.Sing();
+    }
+}
